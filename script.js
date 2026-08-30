d514f8495b5ef4ca12fd57f695e96e1921877761
s1034337 <s1034337@users.noreply.github.com>
2026-08-30 23:08:25 +0800

    Add rounds 8 through 13 question sets

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const PERSONAL_QUIZ_WRONG_CHANCES = 3;
 const PERSONAL_QUIZ_CLEAR_WRONG_LIMIT = 3;
 const PERSONAL_QUIZ_QUESTION_COUNT = 25;
 const ROUND_COUNT = REVIEW_DATA.length;
-const OPEN_ROUNDS_COUNT = 7;
+const OPEN_ROUNDS_COUNT = 13;
 
 function getRoundTotal(roundNum) {
   return RAW_SHEET_DATA.filter(item => item.round === roundNum).length;
